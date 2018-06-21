84d8e0815f9cce2e9b3d0bdee33755b02b7accca
João Marcos <joao_marcos_cota@hotmail.com>
2018-06-21 10:42:27 -0300

    Update ScholarApplication.java

--- backend/scholar/src/main/java/br/ufmg/dcc/scholar/ScholarApplication.java
+++ backend/scholar/src/main/java/br/ufmg/dcc/scholar/ScholarApplication.java
@@ -9,4 +9,24 @@ public class ScholarApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ScholarApplication.class, args);
 	}
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.addAllowedOrigin("*");
+	    config.addAllowedHeader("*");
+	    config.addAllowedMethod("OPTIONS");
+	    config.addAllowedMethod("HEAD");
+	    config.addAllowedMethod("GET");
+	    config.addAllowedMethod("PUT");
+	    config.addAllowedMethod("POST");
+	    config.addAllowedMethod("DELETE");
+	    config.addAllowedMethod("PATCH");
+	    source.registerCorsConfiguration("/**", config);
+	    // return new CorsFilter(source);
+	    final FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+	    bean.setOrder(0);
+	    return bean;
+	}
 }
